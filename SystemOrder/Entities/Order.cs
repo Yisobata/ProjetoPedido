9f5af135bb203f17ea61013e69ba816a57dc9a32
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2020-01-15 21:00:12 -0300

    Criado as classes Order, OrderItem, Client e Product

---
+++ SystemOrder/Entities/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SystemOrder.Entities.Enums;
+using System.Collections.Generic;
+
+namespace SystemOrder.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem items)
+        {
+            Items.Add(items);
+        }
+
+        public void RemoveItem(OrderItem items)
+        {
+            Items.Remove(items);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem items in Items)
+            {
+                sum += items.SubTotal();
+            }
+            return sum;
+        }
+
+    }
+}
