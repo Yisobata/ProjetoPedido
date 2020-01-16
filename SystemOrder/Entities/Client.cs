2382dc67a9d6f7a633eecfbb88b0f98550682469
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2020-01-15 23:03:40 -0300

    Comeco da programacao principal.

--- SystemOrder/Entities/Client.cs
+++ SystemOrder/Entities/Client.cs
@@ -1,10 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SystemOrder.Entities
 {
     class Client
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
     }
 }
--- SystemOrder/Program.cs
+++ SystemOrder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using SystemOrder.Entities;
+using SystemOrder.Entities.Enums;
 
 namespace SystemOrder
 {
@@ -6,7 +8,29 @@ namespace SystemOrder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            DateTime moment = DateTime.Now;
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.WriteLine("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(moment, status, client);
+
+            for (int i = 1; i<=n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+
+            }
         }
     }
 }
